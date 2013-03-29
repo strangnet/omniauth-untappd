f0d78468551edcaac0aec46a9780397363f0074f
Patrick Strang <strangnet@gmail.com>
2013-03-30 00:30:46 +0100

    Adding simplecov and webmock as spec dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec'
 require 'omniauth'
 require 'omniauth-untappd'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.extend OmniAuth::Test::StrategyMacros, :type => :strategy
