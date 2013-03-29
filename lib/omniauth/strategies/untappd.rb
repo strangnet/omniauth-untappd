03e2b91783f0bf892b0a2c8751e16f69a336c720
Patrick Strang <strangnet@gmail.com>
2013-03-30 00:31:46 +0100

    Basic functionality in untappd strategy class

--- lib/omniauth/strategies/untappd.rb
+++ lib/omniauth/strategies/untappd.rb
@@ -7,15 +7,27 @@ module OmniAuth
       option :name, 'untappd'
 
       option :client_options, {
-
+          :site => 'http://untappd.com',
+          :authorize_path => '/oauth/authenticate',
+          :request_token_path => '/oauth/request_token'
       }
 
-      uid { }
+      uid { raw_info['user']['uid'] }
 
       info do
         {
-
+            'uid' => raw_info['user']['id'],
+            'username' => raw_info['user']['user_name'],
+            'url' => raw_info['user']['untappd_url']
         }
+      end
+
+      extra do
+        { :raw_info => raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get('/v4/user/info').body)
       end
     end
   end
