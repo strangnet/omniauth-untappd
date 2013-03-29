da01dbdb9d1845f4986a7d13a6508168882fbe1a
Patrick Strang <strangnet@gmail.com>
2013-03-30 00:31:12 +0100

    simple test for options

--- spec/omniauth/strategies/untappd_spec.rb
+++ spec/omniauth/strategies/untappd_spec.rb
@@ -10,5 +10,13 @@ describe OmniAuth::Strategies::Untappd do
     it 'should have correct name' do
       expect(subject.options.name).to eq 'untappd'
     end
+
+    it 'should have correct site' do
+      expect(subject.options.client_options.site).to eq 'http://untappd.com'
+    end
+
+    it 'should have correct authorize path' do
+      expect(subject.options.client_options.authorize_path).to eq '/oauth/authenticate'
+    end
   end
 end
