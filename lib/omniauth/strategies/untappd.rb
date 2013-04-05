d62927b087433fba66f9ea1d905dbccace345536
Patrick Strang <strangnet@gmail.com>
2013-04-05 21:32:22 +0200

    Trial and error tweaking of params and options to connect to Untappd's API

--- lib/omniauth/strategies/untappd.rb
+++ lib/omniauth/strategies/untappd.rb
@@ -1,14 +1,16 @@
-require 'omniauth-oauth'
+require 'omniauth-oauth2'
 require 'multi_json'
 
 module OmniAuth
   module Strategies
-    class Untappd < OmniAuth::Strategies::OAuth
+    class Untappd < OmniAuth::Strategies::OAuth2
       option :name, 'untappd'
 
       option :client_options, {
-          :site => 'https://untappd.com',
-          :authorize_url => '/oauth/authenticate'
+          site: 'https://untappd.com',
+          authorize_url: '/oauth/authenticate',
+          token_url: '/oauth/authorize',
+          token_method: :get
       }
 
       uid { raw_info['user']['uid'] }
@@ -34,16 +36,37 @@ module OmniAuth
         super
       end
 
+      def callback_phase
+        options[:token_params] = token_client_params.merge(options[:token_params])
+        super
+      end
+
+      def auth_hash
+        OmniAuth::Utils.deep_merge(super, client_params.merge({:code => 'code'}))
+      end
+
       def raw_info
-        @raw_info ||= MultiJson.decode(access_token.get('/v4/user/info').body)
+        access_token.options[:mode] = :query
+
+        @raw_info ||= MultiJson.decode(access_token.get('http://api.untappd.com/v4/user/info').body)
       end
 
       private
 
       def client_params
         {
+            :client_id => options[:client_id],
+            :client_secret => options[:client_secret],
             :redirect_url => callback_url,
             :response_type => 'code'
+        }
+      end
+
+      def token_client_params
+        {
+            :response_type => 'code',
+            :redirect_url => callback_url,
+            :parse => :json
         }
       end
     end
